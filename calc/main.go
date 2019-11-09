78d7b2b9e67901d546c8393072b4535c83dcecb5
Daniel Lwetabe <acehanks@gmail.com>
2019-11-09 17:14:09 +0300

    got add function working

--- calc/main.go
+++ calc/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	gen "github.com/danny/wsdl/calc/myservice"
+	"github.com/hooklift/gowsdl/soap"
+)
 
 func main(){
-	fmt.Println("hello world");
+	client := soap.NewClient("http://www.dneonline.com/calculator.asmx?WSDL")
+	service := gen.NewCalculatorSoap(client)
+	reply, err := service.Add(&gen.Add{ IntA: 2, IntB:5 })
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(reply)
+	// &{{http://tempuri.org/ AddResponse} 7}
 }
